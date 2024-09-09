8cc057cf2c5d582c08361818358b49131f443b4b
younisrajaApplications <younisraja.applications@gmail.com>
2024-09-09 15:19:59 +0100

    Create multiple independentally managed counters

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
 import Counter from './Counter.js';
 
 function App() {
 
-  const [count, setCount] = useState(0);
-
-  const increment = () => setCount(count + 1);
-  const decrement = () => setCount(count - 1);
-
   return (
     <div className="App">
       <h1>Counter Application</h1>
-      <Counter count={count} increment={increment} decrement={decrement}/>
+      <Counter />
+      <Counter />
+      <Counter />
     </div>
   );
 }
--- src/Counter.js
+++ src/Counter.js
@@ -1,11 +1,24 @@
 import React from "react";
+import { useState } from "react";
 
-function Counter({count, increment, decrement}) {
+function Counter() {
+
+    const [count, setCount] = useState(0);
+
+    const increment = () => setCount(count + 1);
+
+    const decrement = () => {
+      if (count > 0) setCount(count - 1);
+    }
+
+    const reset = () => setCount(0);
+
     return (
         <div className="Counter">
             <h2>Count: {count}</h2>
             <button onClick={increment}>Increment</button>
             <button onClick={decrement}>Decrement</button>
+            <button onClick={reset}>Reset</button>
         </div>
     );
 }
